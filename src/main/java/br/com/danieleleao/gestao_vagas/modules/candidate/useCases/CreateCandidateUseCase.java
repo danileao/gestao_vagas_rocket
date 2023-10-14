073e2a7fdec39947d8f641bebd912057787aaeb3
Daniele <dani.leao89@gmail.com>
2023-10-14 08:31:51 -0300

    remove swagger

--- src/main/java/br/com/danieleleao/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/danieleleao/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -6,28 +6,18 @@ import org.springframework.stereotype.Service;
 import br.com.danieleleao.gestao_vagas.exceptions.UserFoundException;
 import br.com.danieleleao.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.danieleleao.gestao_vagas.modules.candidate.CandidateRepository;
-import br.com.danieleleao.gestao_vagas.modules.candidate.dto.CreateCandidateRequestDTO;
 
 @Service
 public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
-    public CandidateEntity execute(CreateCandidateRequestDTO candidateRequestDTO) {
+    public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository
-                .findByUsernameOrEmail(candidateRequestDTO.getUsername(), candidateRequestDTO.getEmail())
+                .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException();
                 });
-
-        var candidateEntity = CandidateEntity
-                .builder()
-                .username(candidateRequestDTO.getUsername())
-                .name(candidateRequestDTO.getName())
-                .password(candidateRequestDTO.getPassword())
-                .email(candidateRequestDTO.getEmail())
-                .description(candidateRequestDTO.getDescription())
-                .build();
 
         return this.candidateRepository.save(candidateEntity);
     }
